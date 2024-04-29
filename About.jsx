ab68206c31c19fbf16dfee2f57aa58df6228e820
KashiReddy31 <126487371+KashiReddy31@users.noreply.github.com>
2024-04-29 21:48:41 +0530

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we are serious about is food.</p>
+            </div>
+            <p className="mid">
+            Welcome to Restaurant Foodie Train, where flavor meets warmth. Discover a culinary haven in the heart of Bhimavarm, where every dish tells a story of passion and quality. Join us for a dining experience that is as memorable as it is delicious.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Ambati Eshwar</div>
+          <div className="right">
+            <p>ADDRESS
+              Bhimavarm
+            </p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Ambati Eshwar</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
